941b71d33ed0ee9c0be39095069353b45983f75a
LAPTOP-EBCJ7681\lenovo <7877290+liuzhi_hao@user.noreply.gitee.com>
2020-10-27 19:20:05 +0800

    一对一

--- src/views/homeRoutes/onevsone.vue
+++ src/views/homeRoutes/onevsone.vue
@@ -19,22 +19,37 @@
         <i></i>
       </div>
     </div>
+
+
     <ul>
       <li :class="n != 1 ? 'none' : ''" class="zmb_time">
         <div class="zmb_time_calendar">
           <van-calendar
+            :min-date="minDate"
+            :max-date="maxDate"
             :poppable="false"
             :show-confirm="false"
-            :show-title="false"
-            :show-subtitle="false"
-            :row-height="'2rem'"
-            :style="{ height: '150px' }"
+            :style="{ height: '500px' }"
           />
+           <div class="lzh-butbox">
+              <van-button
+                type="danger"
+                style="background: #fff; color: #000; border: 1px solid #d9d9d9"
+                >重置</van-button
+              >
+              <van-button
+                type="danger"
+                style="background: #eb6100; color: #fff"
+
+              >
+                确认
+              </van-button>
+            </div>
         </div>
 
         <div class="zmb_time_btns">
-            <div class="zmb_time_btns_reset">重置</div>
-            <div class="zmb_time_btns_submit">确定</div>
+          <div class="zmb_time_btns_reset">重置</div>
+          <div class="zmb_time_btns_submit">确定</div>
         </div>
       </li>
       <li :class="n != 0 ? 'none' : ''" class="zmb_teacher">
@@ -53,7 +68,100 @@
           <button class="zmb_teacher_item_btn">预约</button>
         </div>
       </li>
-      <li :class="n != 2 ? 'none' : ''" class="zmb_list">asdfasdfasdf</li>
+      <li :class="n != 2 ? 'none' : ''" class="zmb_list">
+        <div class="lzh-contbox">
+              <div class="lzh-contai">
+                <p>老师类型</p>
+                <div class="lzh-containera">
+                  <span>M1</span>
+                  <span>M2</span>
+                  <span>M3</span>
+                  <span>M4</span>
+                  <span>M5</span>
+                  <span>M6</span>
+                  <span>M7</span>
+                  <span>M8</span>
+                  <span>M9</span>
+                  <span>M10</span>
+                  <span>M11</span>
+                  <span>M12</span>
+                  <span>M13</span>
+                  <span>M14</span>
+                  <span>M15</span>
+                  <span>M16</span>
+                  <span>M17</span>
+                  <span>M18</span>
+                  <span>M19</span>
+                  <span>M20</span>
+                </div>
+              </div>
+            </div>
+            <div class="lzh-contbox">
+              <div class="lzh-contai">
+                <p>只看</p>
+                <div class="lzh-containera">
+                  <van-checkbox v-model="checked">已关注</van-checkbox>
+                  <van-checkbox v-model="checkedr">上过课</van-checkbox>
+                </div>
+              </div>
+            </div>
+            <div class="lzh-contbox">
+              <div class="lzh-contai">
+                <p>性别</p>
+                <div class="lzh-containera">
+                  <span>男</span>
+                  <span>女</span>
+                </div>
+              </div>
+            </div>
+            <div class="lzh-contbox">
+              <div class="lzh-contai">
+                <p>年级</p>
+                <div class="lzh-containera">
+                  <span>小学一年级</span>
+                  <span>小学二年级</span>
+                  <span>小学三年级</span>
+                  <span>小学四年级</span>
+                  <span>小学五年级</span>
+                  <span>小学六年级</span>
+                  <span>初一</span>
+                  <span>初二</span>
+                  <span>初三</span>
+                  <span>高一</span>
+                  <span>高二</span>
+                  <span>高三</span>
+                </div>
+              </div>
+            </div>
+            <div class="lzh-contbox">
+              <div class="lzh-contai">
+                <p>学科</p>
+                <div class="lzh-containera">
+                  <span>语文</span>
+                  <span>数学</span>
+                  <span>英语</span>
+                  <span>物理</span>
+                  <span>化学</span>
+                  <span>生物</span>
+                  <span>信息技术</span>
+                </div>
+              </div>
+            </div>
+            <div class="lzh-butbox">
+              <van-button
+                type="danger"
+                style="background: #fff; color: #000; border: 1px solid #d9d9d9"
+                >重置</van-button
+              >
+              <van-button
+                type="danger"
+                style="background: #eb6100; color: #fff"
+
+              >
+                确认
+              </van-button>
+            </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -62,6 +170,10 @@
 export default {
   data() {
     return {
+       checked: true,
+      checkedr: true,
+      minDate: new Date(2020, 9, 20),
+      maxDate: new Date(2020, 9, 31),
       teacherList: [
         {
           img:
@@ -245,35 +357,74 @@ body {
 }
 .zmb_list {
   width: 100%;
-  height: 100px;
-  background: darkgreen;
+  background: #fff;
+  position: relative;
 }
-.zmb_time_calendar{
-    width: 95%;
-    height: 150px;
-    margin: 0.5rem auto;
+.zmb_time_calendar {
+  width: 100%;
+  margin: 0.5rem auto;
 }
-.zmb_time_btns{
-    width: 100%;
-    height: 2rem;
-    display: flex;
-    position: fixed;
-    bottom: 0px;
-    left: 0;
+.zmb_time_btns {
+  width: 100%;
+  height: 2rem;
+  display: flex;
+  position: fixed;
+  bottom: 0px;
+  left: 0;
 }
-.zmb_time_btns div{
-    width: 50%;
-    height: 100%;
-    line-height: 2rem;
-    text-align: center;
-    font-size: 0.85rem;
+.zmb_time_btns div {
+  width: 50%;
+  height: 100%;
+  line-height: 2rem;
+  text-align: center;
+  font-size: 0.85rem;
 }
-.zmb_time_btns .zmb_time_btns_reset{
-    background: #EB6100;
-    color: white;
+.zmb_time_btns .zmb_time_btns_reset {
+  background: #eb6100;
+  color: white;
 }
-.zmb_time_btns .zmb_time_btns_submit{
-    color: #EB6100;
-    background: white;
+.zmb_time_btns .zmb_time_btns_submit {
+  color: #eb6100;
+  background: white;
+}
+.lzh-contbox {
+  width: 100%;
+  font-size: 0.3rem;
+}
+.lzh-contai {
+  border-bottom: 1px solid #f1f1f1;
+  margin-bottom: 0.5rem;
+  margin-left: 1rem;
+}
+.lzh-containera {
+  width: 100%;
+  margin-top: 0.2rem;
+  display: inline-flex;
+  justify-content: left;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.lzh-containera span {
+  width: 3rem;
+  height: 1.5rem;
+  line-height: 1.5rem;
+  background: #f5f5f5;
+  text-align: center;
+  margin-right: 0.7rem;
+  margin-bottom: 0.8rem;
+}
+.lzh-butbox {
+  width: 100%;
+  height: 3rem;
+  display: inline-flex;
+  justify-content: space-around;
+  align-items: center;
+  z-index: 9;
+  position: fixed;
+  bottom: 0;
+}
+.lzh-butbox button {
+  width: 6rem;
+  height: 1.3rem;
 }
 </style>
