175bad36e330a92f6d07fc99b90cfd33fb9310f2
张梅波 <8041535+zhangmeibo@user.noreply.gitee.com>
2020-10-27 14:16:41 +0800

    zmb

---
+++ src/components/FT_title.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <p class="FT_title">精品课堂</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.FT_title {
+  width: 100%;
+  font-family: PingFangSC-Medium;
+  font-weight: 500;
+  color: #595959;
+  padding-left: 0.72rem;
+  margin: 0.5rem 0.72rem;
+  line-height: 1rem;
+  height: 1rem;
+  font-size: 0.7rem;
+  border-left: 3px solid #EB6100;
+}
+</style>
