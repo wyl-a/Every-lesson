fd34a7147b3d585c740645b6c5fb8da5b1b9e6e9
王鹏飞飞 <8033685+wang-peng-fei@user.noreply.gitee.com>
2020-10-27 20:35:00 +0800

    王鹏飞飞

--- src/apl/index.js
+++ src/apl/index.js
@@ -4,9 +4,13 @@ import {
 import apl from "../apl/path"
 
 const APP = {
+    //方法
     banner(){
         return request(apl.METHODS.GET,apl.URL.Banner);
-    }
+    },
+    getteacher(){
+      return  request(apl.METHODS.GET,apl.URL.GETteacher,{});
+    },
 }
 
 export default APP
