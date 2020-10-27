cfc1ffebf664c09b58fa5a3f7ae661933a604917
张梅波 <8041535+zhangmeibo@user.noreply.gitee.com>
2020-10-27 18:56:13 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,16 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [{
+const routes = [
+  {
     path: '/',
     name: 'home',
     component: () => import('../views/Home.vue'),
     meta: {
       title: "首页",
-<<<<<<< HEAD
       isShow:false,
       isTab:true,
       email:true,
-=======
-      isShow: false,
-      isTab: true,
-      email: true
->>>>>>> bc84e8a2f642dd084fefcef3cc43365c2687364f
     }
   },
   {
@@ -26,55 +21,34 @@ const routes = [{
     component: () => import('../views/course/Course.vue'),
     meta: {
       title: "课程",
-<<<<<<< HEAD
       isShow:true,
       isShowleft:false,
       isTab:true,
       email:true,
-=======
-      isShow: true,
-      isShowleft: false,
-      isTab: true,
-      email: true
->>>>>>> bc84e8a2f642dd084fefcef3cc43365c2687364f
     }
   },
-  {
+   {
     path: '/appointment',
     name: 'Appointment',
     component: () => import('../views/Appointment.vue'),
     meta: {
       title: "约课记录",
-<<<<<<< HEAD
       isShow:true,
       isShowleft:false,
       isTab:true,
       email:true,
-=======
-      isShow: true,
-      isShowleft: false,
-      isTab: true,
-      email: true
->>>>>>> bc84e8a2f642dd084fefcef3cc43365c2687364f
     }
-  },
+  }, 
   {
     path: '/practice',
     name: 'Practice',
     component: () => import('../views/Practice.vue'),
     meta: {
       title: "练习",
-<<<<<<< HEAD
       isShow:true,
       isShowleft:false,
       isTab:true,
       email:true,
-=======
-      isShow: true,
-      isShowleft: false,
-      isTab: true,
-      email: true
->>>>>>> bc84e8a2f642dd084fefcef3cc43365c2687364f
     }
   },
   {
@@ -83,17 +57,10 @@ const routes = [{
     component: () => import('../views/User.vue'),
     meta: {
       title: "我的",
-<<<<<<< HEAD
       isShow:true,
       isShowleft:false,
       isTab:true,
       email:true,
-=======
-      isShow: true,
-      isShowleft: false,
-      isTab: true,
-      email: true
->>>>>>> bc84e8a2f642dd084fefcef3cc43365c2687364f
     }
   },
   //一对一课程
@@ -103,19 +70,9 @@ const routes = [{
     component: () => import('../views/homeRoutes/onevsone.vue'),
     meta: {
       title: "一对一课程",
-      isShow: true,
-      isShowleft: true,
-      isTab: false,
-      email: true
-    }
-  },
-  {
-    path: '/email',
-    name: 'Email',
-    component: () => import('../views/mailBox/email.vue'),
-    meta: {
-      isTab: false,
-      email: false
+      isShow:true,
+      isShowleft:true,
+      isTab:false,
     }
   },
   {
