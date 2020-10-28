4bae3b2072ad6126c1e017d0ec3b6adb08799b0b
张梅波 <8041535+zhangmeibo@user.noreply.gitee.com>
2020-10-28 15:56:42 +0800

    no message

--- src/apl/path.js
+++ src/apl/path.js
@@ -1,20 +1,16 @@
 const apl = {
     METHODS: {
-        GET:"get",  //get请求
+        GET: "get", //get请求
         POST: "post" //post请求
     },
     //接口路径
     URL: {
-        //轮播图路径
-
-      Banner:'/api/app/banner',
-
         //首页列表老师
         GETteacher: '/api/app/recommend/appIndex',
         //课程
         course: '/api/app/courseBasis?page=1&limit=10&',
         //登录
-        Login:"/api/app/login",
+        Login: "/api/app/login",
     }
 }
 export default apl
